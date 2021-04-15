0ac49c4f5b2fa4079857209cba33b5a5d05118ba
Astro Girl <>
2021-04-15 20:51:48 +0000

    It no longer runs into walls!!!!!!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //HTTP credits: http://lcs.ios.ac.cn/~maxt/SPelton/reports/report-9aa0d3.html
 //JSON credits: https://github.com/nlohmann/json
 #include <iostream>
+#include <vector>
 #include "./json.hpp"
 #include "./http_stuff.h"
 using namespace std;
@@ -8,10 +9,10 @@ using namespace nlohmann;
 int main(void) {
   httplib::Server svr;
   svr.Get("/", [](const auto &, auto &res) {
-    string head = "default"; //TODO: Change head
-    string tail = "default"; //TODO: Change tail
+    string head = "shades"; //TODO: Change head
+    string tail = "freckled"; //TODO: Change tail
     string author = ""; //TODO: Change your battlesnake username
-    string color = "#888888";  //TODO: Change a hex color
+    string color = "#ffaa00";  //TODO: Change a hex color
     res.set_content("{\"apiversion\":\"1\", \"head\":\"" + head + "\", \"tail\":\"" + tail + "\", \"color\":\"" + color + "\", " + "\"author\":\"" + author + "\"}", "text/json");
   });
   svr.Post("/end", [](const auto &, auto &res){
@@ -30,9 +31,36 @@ int main(void) {
     //You might need to make some structs to store some data in a variable
     //Example:
     //you_struct you = data["you"];
+
+    vector <string> movevec;
     string moves[4] = {"up", "down", "left", "right"};
     int index = rand() % 4;
-    res.set_content("{\"move\": \"" + moves[index] + "\"}", "text/plain");
+    const json you = data["you"];
+    int headx = you["head"]["x"];
+    int heady = you["head"]["y"];
+    int width = data["board"]["width"];
+    int height = data["board"]["height"];
+
+    string move = "";
+    if (headx != 0){
+      movevec.push_back( "left");
+    }
+    if (heady != 0){
+      movevec.push_back("down");
+    }
+    if (headx < width - 1){
+      movevec.push_back("right");
+    }
+    if (heady < height - 1){
+      movevec.push_back("up");
+    }
+    if (movevec.size() > 0){
+      move = movevec[rand() % movevec.size()];
+    }else{
+      move = moves[rand() % 4];
+    }
+
+    res.set_content("{\"move\": \"" + move + "\"}", "text/plain");
   });
   svr.listen("0.0.0.0", 8080);
   cout << "Server started";
